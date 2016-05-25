b7cc3494f33f45f6683e728c10bb045bccc16afe
canlasd <canlasd@aol.com>
2016-05-25 15:12:48 -0400

    fixed android standalone rendering failures.  Added buttons to main activity

--- app/src/main/java/canlasd/example/com/youtubeplayer/MainActivity.java
+++ app/src/main/java/canlasd/example/com/youtubeplayer/MainActivity.java
@@ -1,28 +1,34 @@
 package canlasd.example.com.youtubeplayer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnSingle;
+    private Button btnStandAlone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        btnSingle = (Button) findViewById(R.id.btnPlaySingle);
+        btnStandAlone = (Button) findViewById(R.id.btnStandAlone);
+
+        btnSingle.setOnClickListener(this);
+        btnStandAlone.setOnClickListener(this);
+
+
     }
 
     @Override
@@ -34,16 +40,36 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+
+        switch (v.getId()) {
+            case R.id.btnPlaySingle:
+                intent = new Intent(MainActivity.this, YouTubeActivity.class);
+                break;
+
+            case R.id.btnStandAlone:
+                intent = new Intent(MainActivity.this, StandAloneActivity.class);
+                break;
+
+            default:
+        }
+
+        if (intent != null) {
+            startActivity(intent);
+        }
+
     }
 }
--- app/src/main/java/canlasd/example/com/youtubeplayer/StandAloneActivity.java
+++ app/src/main/java/canlasd/example/com/youtubeplayer/StandAloneActivity.java
@@ -13,7 +13,8 @@ public class StandAloneActivity extends AppCompatActivity implements View.OnClic
 
     private String GOOGLE_API_KEY = "AIzaSyCOCX2a1mULZKtgaSmZu_xLj5JePl9IwQE";
     private String YOUTUBE_VIDEO_ID = "eHevNn-AVQg";
-    private String YOUTUBE_PLAYLIST = "PLhRy0N4DxIyBXzUGKzUSdZ1qVDoP7-HHJ";
+    private String YOUTUBE_PLAYLIST = "PLXtTjtWmQhg3aTiE0y_iO_DRlKtTiv519";
+
     private Button btnPlayVideo;
     private Button btnPlayList;
 
@@ -31,14 +32,6 @@ public class StandAloneActivity extends AppCompatActivity implements View.OnClic
         btnPlayList.setOnClickListener(this);
         btnPlayVideo.setOnClickListener(this);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
     }
 
     @Override
@@ -53,7 +46,7 @@ public class StandAloneActivity extends AppCompatActivity implements View.OnClic
                 break;
 
             case R.id.btnPlayerList:
-                intent = YouTubeStandalonePlayer.createVideoIntent(this, GOOGLE_API_KEY,
+                intent = YouTubeStandalonePlayer.createPlaylistIntent(this, GOOGLE_API_KEY,
                         YOUTUBE_PLAYLIST);
                 break;
 
